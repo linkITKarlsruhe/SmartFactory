b91f070ff00dcf13d7f7c21f3cf5080d8ed0ef2f
mariohaag <mario.haag@linkit.tech>
2018-05-05 12:36:37 +0200

    Docu 05052018

--- 01_CentralControl/Software/EclipseProjectNRF24/SmartNrf/build/ArduinoNano2/core/wiring.c.d
+++
@@ -1,7 +0,0 @@
-core/wiring.c.o: \
- /home/mario/.arduinocdt/packages/arduino/hardware/avr/1.6.20/cores/arduino/wiring.c \
- /home/mario/.arduinocdt/packages/arduino/hardware/avr/1.6.20/cores/arduino/wiring_private.h \
- /home/mario/.arduinocdt/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h \
- /home/mario/.arduinocdt/packages/arduino/hardware/avr/1.6.20/cores/arduino/binary.h \
- /home/mario/.arduinocdt/packages/arduino/hardware/avr/1.6.20/variants/eightanaloginputs/pins_arduino.h \
- /home/mario/.arduinocdt/packages/arduino/hardware/avr/1.6.20/variants/eightanaloginputs/../standard/pins_arduino.h
