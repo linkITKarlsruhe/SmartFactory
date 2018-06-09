435dacd3cde840d53290ee028e6f7c051f3924d9
mariohaag <mario.haag@linkit.tech>
2018-06-09 21:18:08 +0200

    Staunen

--- 01_CentralControl/Software/LinkitRF24Capsel/lib/LinkitRF24/LinkitRF24.h
+++ 01_CentralControl/Software/LinkitRF24Capsel/lib/LinkitRF24/LinkitRF24.h
@@ -11,13 +11,8 @@
 class LinkitRF24{
 
 private:
-  // RF24 radio;
-  // RF24Network network;
-  // RF24Mesh mesh;
-  // RF24EthernetClass RF24Ethernet;
   char *clientID;
 public:
-  // LinkitRF24(): radio(7,8),network(radio),mesh(radio,network),RF24Ethernet(radio,network,mesh){}
   void setup(MQTT_CALLBACK_SIGNATURE,char* );
   bool meshBegins();
   bool mqttConnected();
@@ -28,10 +23,4 @@ public:
   bool clientLoop();
 };
 
-// class PubSubClient;
-// class SPI;
-// class RF24;
-// class RF24Network;
-// class RF24Ethernet;
-//  class RF24Mesh;
 #endif
